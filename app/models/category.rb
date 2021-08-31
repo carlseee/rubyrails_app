cf42d84a36fe11a9991966b040f489475ed7daf2
Carl See <carlsee06@gmail.com>
2021-08-31 20:45:06 +0800

    pushing html and css

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
     validates :name, presence: true, uniqueness: true 
-    validates :description, presence: true, length: { minimum: 5}  
+    validates :description, presence: true
 
     has_many :tasks
 end
